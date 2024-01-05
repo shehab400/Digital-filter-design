44c84eb75a3af126c07d482796836346fd1b5b85
M7mdddibrahim <m7mdalhussini@gmail.com>
2024-01-05 15:24:09 +0200

    new GUI

--- MyWindow.py
+++ MyWindow.py
@@ -8,3 +8,15 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         self.ui = uic.loadUi("GUI.ui", self)
+
+        ## Change Qpushbutton Checkable status when stackedWidget index changed  
+    def stackedWidget_currentChanged (self, index):
+        btn_list = self.ui.icon_only.findChild(QPushButton) \
+        + self.ui.full_menu.findChild(QPushButton)
+
+        for btn in btn_list:
+            if index in [5,6]:
+                btn.setAutoExclusive(False)
+                btn.setChecked(False)
+            else:
+                btn.setAutoExclusive(True)
